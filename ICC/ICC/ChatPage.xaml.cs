12d0336bf08012fb044ea1fe496c45698949a32c
amcfarland45 <38435078+amcfarland45@users.noreply.github.com>
2019-04-18 09:00:17 -0600

    Added design to home page

--- ICC/ICC/ChatPage.xaml.cs
+++ ICC/ICC/ChatPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace ICC
 {
@@ -16,5 +17,31 @@ namespace ICC
 		{
 			InitializeComponent ();
 		}
-	}
+
+        /*public async Task SendSms(string messageText, string recipient)
+        {
+            try
+            {
+                var message = new SmsMessage(messageText, new[] { recipient });
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+        public async void OnEntryCompleted(object sender, EventArgs arg)
+        {
+            Entry data = (Entry)sender;
+            string text = data.Text;
+            string textRecipient = "+12083585398";
+            string emailRecipient = "andrewmcfarland@mycwi.cc";
+            await SendSms(text, textRecipient);
+            //call to email function(text, emailRecipient);
+        }*/
+    }
 }
