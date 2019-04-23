bea149396d31483f49795496888cdeb54b5594e9
Ben Cowan <imbencowan@gmail.com>
2019-04-23 08:30:48 -0600

    implemented email on service request submit

--- ICC/ICC/RequestServiceFormPage.xaml.cs
+++ ICC/ICC/RequestServiceFormPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace ICC
 {
@@ -17,9 +18,50 @@ namespace ICC
             InitializeComponent();
         }
 
-        void OnClickSubmit(object sender, EventArgs e)
+        async void OnClickSubmit(object sender, EventArgs e)
         {
-            this.Navigation.PushModalAsync(new ServiceConfirmationPage());
+            List<string> address = new List<string>();
+            address.Add("imbencowan20@gmail.com");
+            address.Add("2089215438@vtext.com");
+
+            string message = "";
+            message += "Request from: \n";
+            message += EntryFName.Text + " " + EntryLName.Text + "\n";
+            message += EntryEmail.Text + "\n";
+            message += EntryPhone.Text + "\n";
+            message += EntryAddress1.Text + "\n";
+            message += EntryAddress2.Text + "\n";
+            message += EntryCity.Text + ", " + EntryState.Text + " " + EntryZip;
+
+
+            await SendEmail("Test", message, address);
+            
+            //this.Navigation.PushModalAsync(new ServiceConfirmationPage());
+        }
+        
+        public async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                    //Cc = ccRecipients,
+                    //Bcc = bccRecipients
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+            }
+            
         }
     }
 }
