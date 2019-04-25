f74dd05449d5ec700360e4f6fd6ec4d6b022c33e
Ben Cowan <imbencowan@gmail.com>
2019-04-25 10:11:23 -0600

    linked buttons on the services page

--- ICC/ICC/ServicesPage.xaml.cs
+++ ICC/ICC/ServicesPage.xaml.cs
@@ -19,37 +19,42 @@ namespace ICC
 
         private void Relocation_Services(object sender, EventArgs e)
         {
-
+            RequestService("Relocation Services");
         }
 
         private void Corporate_Events(object sender, EventArgs e)
         {
-
+            RequestService("Corporate Event");
         }
 
         private void Errand_Running(object sender, EventArgs e)
         {
-
+            RequestService("Errand Running");
         }
 
         private void Temporary_Assisants(object sender, EventArgs e)
         {
-
+            RequestService("Temporary Assistant");
         }
 
         private void AccommodationsBTN(object sender, EventArgs e)
         {
-
+            RequestService("Accomodations");
         }
 
         private void Business_Retreats(object sender, EventArgs e)
         {
-
+            RequestService("Business Retreat");
         }
 
         void btn_Clicked(object sender, EventArgs e)
         {
             //this.Navigation.PushAsync(new RequestServiceFormPage());
+        }
+
+        void RequestService(string service)
+        {
+            this.Navigation.PushModalAsync(new RequestServiceDatePage(service));
         }
 
         void OnClickTest(object sender, EventArgs e)
